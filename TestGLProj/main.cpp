17448172a1aa06c70bc057d409658dd63cc9732a
Rakerd Calhoun <RakerdC@gmail.com>
2021-05-09 16:31:25 -0500

    Added road collision detection
    It's just checking if the boxes collide, and since I don't have the exact dimensions I took guess-measurements. I tried to annotate the code as best I could. Could add a death-threshold after a certain fall height is reached?

--- TestGLProj/main.cpp
+++ TestGLProj/main.cpp
@@ -18,10 +18,10 @@
 
 #define M_PI 3.1415926535897932384626433832795
 
-void collisionDetection(glm::vec3 pos, float radius);
-void collisionResolution();
+void collisionDetectionSphere(glm::vec3 pos, float radius);
+void collisionResolutionSphere();
 void generateCarBox();
-bool roadCheck(glm::vec3 roadPos, float roadXWidth, float roadZLength);
+bool roadCheck(glm::vec3 roadPos, float roadXWidth, float roadYHeight, float roadZLength);
 
 Shader shader; // loads our vertex and fragment shaders
 //Shader3 shaderSL; // loads our vertex and fragment shaders
@@ -34,8 +34,8 @@ Model *lamp; //a lamp-post
 glm::mat4 projection; // projection matrix
 glm::mat4 view; // where the camera is looking
 glm::mat4 model; // Main-Body Model
-glm::vec3 carBoxMinXZ; //Contains the minimum x and z values for the car's top-down 2D bounding box.
-glm::vec3 carBoxMaxXZ; //Contains the maximum x and z values for the car's top-down 2D bounding box.
+glm::vec3 carBoxMinXYZ; //Contains the minimum x, y, and z values for the car's top-down 2D bounding box.
+glm::vec3 carBoxMaxXYZ; //Contains the maximum x, y, and z values for the car's top-down 2D bounding box.
 float angle = 0; //The angle of the car
 glm::vec3 position = glm::vec3(0.0f, 1.0f, 0.0f); //The position of the player/car
 glm::vec3 direction; //The direction the car is facing.
@@ -46,8 +46,10 @@ float camAngle;
 float camPitch = 0.0f;
 float speed = 0.0f; //Value of the car's current speed multiplier.
 int onRoad = 0; //Increments if the car is currently on a road. Total is the number of road tiles the car is concurrently on.
+int isGravity = 1; //1 for gravity to be turned on, 0 for gravity to be turned off (such as when currently driving on the road)
 
-const float CAR_COLLIDER_RADIUS = 1.0f;
+const float CAR_COLLIDER_RADIUS = 1.0f; //The radius of the car's collider sphere for obstacle collision.
+const float GRAVITY_SPEED = 0.02f; // Speed at which the car will fall when not on the road.
 const float MAX_SPEED = 1.0f; //Maxiumum speed multiploer the car can reach.
 const float ACCELERATION = 0.01f; //Change in speed when w or s are held
 const float HANDBREAK_STRENGTH = 0.1f; //Change in speed when Spacebar is held
@@ -121,42 +123,48 @@ void display(void)
 		view = glm::lookAt(camPos, camPos+camDir, glm::vec3(0.0f, 1.0f, 0.0f));
 	} 
 	else if(cameraMode == 1) //First Person
+		//The camera fix to allow turning was adding direction to the eye location since you moved the turnpoint to be on the front axel.
 		view = glm::lookAt(position + direction + glm::vec3(0.0f, 1.0f, 0.0f), position+ (-direction)+glm::vec3(0.0f, 1.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));//camera render location
 	else if(cameraMode == 2) //Top Down
 		view = glm::lookAt(position + glm::vec3(0.0f, 20.0f, 0.0f), position + (-direction) + glm::vec3(0.0f, 1.0f, 0.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 	else if(cameraMode == 3) //Third Person
+		//Is there a better way to do +direction*5, yeah but I put this. Cahnge it if you feel like it.
 		view = glm::lookAt(position + direction + direction + direction + direction + direction + glm::vec3(0.0f, 2.0f, 0.0f), position + (-direction) + glm::vec3(0.0f, 1.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 
 	car->render(view *model * glm::scale(1.0f, 1.0f, 1.0f) * glm::translate(0.0f,0.0f,1.0f), projection); // Render car
 	//plane->render(view * glm::translate(0.0f,0.0f,-175.0f)*glm::scale(10.0f,1.0f,180.0f), projection);
+	/*This section will check if the car is driving on any of the roads*/
 	onRoad = 0;
 	for (int i = 0; i < 350; i += 20) {
 		street->render(view * glm::translate(-3.0f, 0.0f, 0.0f - i) * glm::scale(2.0f, 2.0f, 2.0f), projection);
-		if (roadCheck(glm::vec3(-3.0f, 0.0f, 0.0f-i), 19.0f, 19.0f)) {
+		if (roadCheck(glm::vec3(-3.0f, 0.0f, 0.0f-i), 19.0f, 0.5f, 19.0f)) { //These road-values besides position are approximations due to lack of exact measurements.
 			onRoad++;
 		}
 	}
-	if (onRoad == 0) {
+	if (onRoad == 0) { //This if statement is if the car is not on any roads.
 		printf("Not on road. ");
-		//TODO: Falling or other punishment if car is not on the track
+		isGravity = 1;
+		//TODO: This is where you will call things for if the car is not on ANY road. Falling or other punishment if car is not on the track for example.
+	} else { //This if statement is if the car is on at least one road.
+		isGravity = 0;
 	}
 	
 	for (int i = 0; i < 350; i += 20) {
 		//Add lamps on Right hand side
 		lamp->render(view * glm::translate(5.0f, 0.0f, 0.0f - i) * glm::scale(2.0f, 2.0f, 2.0f), projection);
 		//Test collision
-		collisionDetection(glm::vec3(5.0f, 0.0f, 0.0f-i), .7f);
+		collisionDetectionSphere(glm::vec3(5.0f, 0.0f, 0.0f-i), .7f);
 		// TODO: Better establish the base of the lamp's coordinates so that the collision spheres are exactly at 
 		// the base of the lamp so you don't collide in the space underneath the light which is techinally the middle of the lamp model.
 		//Add lamps on Left hand side
 		lamp->render(view * glm::translate(-12.0f, 0.0f, 0.0f - i)* glm::rotate(180.0f, 0.0f, 1.0f, 0.0f) * glm::scale(2.0f, 2.0f, 2.0f), projection);
 		//Test collision
-		collisionDetection(glm::vec3(-12.0f, 0.0f, 0.0f-i), .7f);
+		collisionDetectionSphere(glm::vec3(-12.0f, 0.0f, 0.0f-i), .7f);
 		// TODO: Better establish the base of the lamp's coordinates so that the collision spheres are exactly at 
 		// the base of the lamp so you don't collide in the space underneath the light which is techinally the middle of the lamp model.
 	}
 
-	position -= direction * speed;
+	position -= (direction * speed) + glm::vec3(0.0f, isGravity * GRAVITY_SPEED, 0.0f); //Now includes gravity Effects
 
 	//lightPosition = glm::rotate(0.1f, 0.0f, 0.0f, 1.0f) * lightPosition;
 	//shaderSL.Activate(); // Bind shader.
@@ -185,14 +193,14 @@ void display(void)
 }
 
 /*Put in the location and radius for the collision sphere of the object for the car to test against. If a collision is detected collisionResolution is automatically called.*/
-void collisionDetection(glm::vec3 pos, float radius) {
+void collisionDetectionSphere(glm::vec3 pos, float radius) {
 	if (glm::distance(position, pos) < CAR_COLLIDER_RADIUS + radius) {
-		collisionResolution();
+		collisionResolutionSphere();
 	}
 }
 
 /*Automatically called if collisionDetection detects the car's collision sphere has entered another collision sphere.*/
-void collisionResolution() {
+void collisionResolutionSphere() {
 	if (speed != 0) {
 		printf("Hit something. ");
 		//Sets speed to 0 after negating the movement that lead to being stuck to ensure they remain where they were last safe instead of just slamming into the wall immediately again.
@@ -209,25 +217,43 @@ void collisionResolution() {
 
 /*Regenerates the cars simple bounding box based on it's current position and the radius of the sphere-collider used for object collision*/
 void generateCarBox() {
-	carBoxMaxXZ = glm::vec3(position.x + CAR_COLLIDER_RADIUS, position.y, position.z + CAR_COLLIDER_RADIUS);
-	carBoxMinXZ = glm::vec3(position.x - CAR_COLLIDER_RADIUS, position.y, position.z - CAR_COLLIDER_RADIUS);
+	//Approxiamtion due to lack of car-measurements
+	carBoxMaxXYZ = glm::vec3(position.x + CAR_COLLIDER_RADIUS, position.y+1.0f, position.z + CAR_COLLIDER_RADIUS);
+	carBoxMinXYZ = glm::vec3(position.x - CAR_COLLIDER_RADIUS, position.y, position.z - CAR_COLLIDER_RADIUS);
 }
 
-/*Will return true if the car is currently within the road's bounding box, which this method also generates.*/
-bool roadCheck(glm::vec3 roadPos, float roadXWidth, float roadZLength) {
-	//Generate Road bounding values
-	glm::vec3 roadMaxXZ(roadPos.x + (roadXWidth / 2), roadPos.y, roadPos.z + (roadZLength / 2));//The coordinate that contains the Max X and Max Z of this road tile.
-	glm::vec3 roadMinXZ(roadPos.x - (roadXWidth / 2), roadPos.y, roadPos.z - (roadZLength / 2));//The coordinate that contains the Min X and Min Z of this road tile.
+/*Will return false if the car is not currently on the road, true if the car is currently colliding on the road.*/
+bool roadCheck(glm::vec3 roadPos, float roadXWidth, float roadYHeight, float roadZLength) {
+	//Generate Road bounding values, 
+	glm::vec3 roadMaxXYZ(roadPos.x + (roadXWidth / 2), roadPos.y+roadYHeight, roadPos.z + (roadZLength / 2));//The coordinate that contains the Max X and Max Z of this road tile.
+	glm::vec3 roadMinXYZ(roadPos.x - (roadXWidth / 2), roadPos.y, roadPos.z - (roadZLength / 2));//The coordinate that contains the Min X and Min Z of this road tile.
 	//Test corners for inclusion in box
-	if ((carBoxMaxXZ.x <= roadMaxXZ.x && carBoxMaxXZ.x >= roadMinXZ.x //If car's max X is within road box
+	if ((carBoxMaxXYZ.x <= roadMaxXYZ.x && carBoxMaxXYZ.x >= roadMinXYZ.x //If car's max X is within road box
 		||															//Or
-		carBoxMinXZ.x <= roadMaxXZ.x && carBoxMinXZ.x >= roadMinXZ.x) //If car's min X is within road box
+		carBoxMinXYZ.x <= roadMaxXYZ.x && carBoxMinXYZ.x >= roadMinXYZ.x) //If car's min X is within road box
 		&&																//AND
-		(carBoxMaxXZ.z <= roadMaxXZ.z && carBoxMaxXZ.z >= roadMinXZ.z //If car's max Z is within road box
+		(carBoxMaxXYZ.z <= roadMaxXYZ.z && carBoxMaxXYZ.z >= roadMinXYZ.z //If car's max Z is within road box
 		||															//Or
-		carBoxMinXZ.z <= roadMaxXZ.z && carBoxMinXZ.z >= roadMinXZ.z))//If car's min Z is within road box
+		carBoxMinXYZ.z <= roadMaxXYZ.z && carBoxMinXYZ.z >= roadMinXYZ.z))//If car's min Z is within road box
 	{
-		return true; //If a corner is within the road's box
+		if (carBoxMinXYZ.y == roadMaxXYZ.y) { //On the road face still
+			return true;//Gravity Off (driving on road)
+		}
+		if (carBoxMinXYZ.y > roadMaxXYZ.y || carBoxMaxXYZ.y < roadMinXYZ.y) { //If the car is above the road-box or under the roadbox
+			return false;//Gravity On (not colliding with road)
+		}
+		if (carBoxMinXYZ.y > roadMinXYZ.y && carBoxMinXYZ.y < roadMaxXYZ.y){ //If bottom of car is colliding with the road
+			position += glm::vec3(0.0f, roadMaxXYZ.y - carBoxMinXYZ.y, 0.0f);
+			return true;//Gravity Off (collision with road resolved, now driving on road)
+		}
+		if (carBoxMaxXYZ.y > roadMinXYZ.y && carBoxMaxXYZ.y < roadMaxXYZ.y) {//Checking if top of car is colliding with road
+			//Since the last if statement would have returned if true this means the car is bumping into the bottom of the road so we will push it beneath the road
+			position -= glm::vec3(0.0f, carBoxMaxXYZ.y - roadMinXYZ.y, 0.0f);
+			return false;//Gravity On (collision with road resolved, not driving on the road)
+		}
+		printf("ERROR: This is not supposed to be reachable. \n roadCheck unresolved case. \n carBoxMaxXYZ.y = %f \n carBoxMinXYZ.y = %f \n roadBoxMaxXYZ.y = %f \n roadBoxMinXYZ.y = %f",
+			carBoxMaxXYZ.y, carBoxMinXYZ.y, roadMaxXYZ.y, roadMinXYZ.y);
+		return true; //Shouldn't be reachable but if it is the error message above will print diagnostics.
 	} else {
 		return false; //If no corner is within the road's box
 	}
